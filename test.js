beec6b38951b43108d28aef664c577f3e194711e
Easton Gorishek <ieaston26@gmail.com>
2018-05-07 11:02:51 -0700

    created files

---
+++ test.js
@@ -0,0 +1,7 @@
+// AC #3 function return remainder
+if(dividend % divisor === result) {
+    console.log('TDD-AC3 - function return correct result - SUCCESS'); 
+}
+else {
+    console.error('TDD-AC3 - function return correct result - FAIL');
+}
